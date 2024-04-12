2b5c4a25081474627a2e060081d6f48dacb582cc
shalinipsg <shalucheru@gmail.com>
2024-04-11 23:57:21 -0400

    history service

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # history_service
+team: shacheru, kparuch
---
+++ src/main/java/edu/iu/shacheru/historyservice/controllers/PrimesHistoryController.java
@@ -0,0 +1,27 @@
+package edu.iu.shacheru.historyservice.controllers;
+
+import edu.iu.shacheru.historyservice.model.PrimesRecord;
+import edu.iu.shacheru.historyservice.repository.PrimesHistoryRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/primes")
+public class PrimesHistoryController {
+
+    private final PrimesHistoryRepository primesHistoryRepository;
+
+    public PrimesHistoryController(
+            PrimesHistoryRepository primesHistoryRepository){
+
+        this.primesHistoryRepository = primesHistoryRepository;
+    }
+
+    @GetMapping("/{customer}")
+    public List<PrimesRecord> findAll(@PathVariable String customer){
+        return primesHistoryRepository.findAllByCustomer(customer);
+    }
+
+}
